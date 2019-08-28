a05901151c9c9f80b1fe86667fc36dd318267c1e
Mia Moore <miaevt@gmail.com>
2019-08-28 05:03:42 -0500

    Remove commented out code and update print out

--- Newsdb.py
+++ Newsdb.py
@@ -16,34 +16,14 @@ if __name__ == '__main__':
     errors = c.fetchall()
     db.close()
 
-    print("What are the most popular three articles of all time?\n")
+    print("Three most popular articles of all time:\n")
     for x in articles:
         print("{} - {} views".format(x[0], x[1]))
 
-    print("\nWho are the most popular article authors of all time?\n")
+    print("\nMost popular article authors of all time:\n")
     for x in authors:
         print("{} - {} views".format(x[0], x[1]))
 
-    print("\nOn which days did more than 1% of requests lead to errors?\n")
+    print("\nDays with more than 1% of error requests:\n")
     for x in errors:
-        print("{} - {:.2f}% errors".format(x[0], x[1]))
-
-    
-    #{}\nWho are the most popular article authors of all time?\n{}\nOn which days did more than 1% of requests lead to errors?\n{}".format(articles, authors, errors))
-
-  
-
-# Question 1 query
-#select articles.title, logpath.views from (select path, count(*) as views from log where status = '200 OK' and path != '/' group by path) 
-#as logpath join articles on logpath.path like concat('%', articles.slug, '%') order by logpath.views desc limit 3;
-
-# Question 2 query
-#select authors.name, sum(logpath.logviews) as views from (select path, count(*) as logviews from log where status = '200 OK' and path != '/' group by path)
-#as logpath join articles on logpath.path like concat('%', articles.slug, '%') join authors on articles.author = authors.id
-#group by authors.name order by views desc;
-
-# Question 3 query
-#select errtotal.time::timestamp::date, cast(errtotal.error as float) / cast(total.requests as float) * 100 as errors
-#from (select time::timestamp::date, count(*) as error from log where status = '404 NOT FOUND' group by time::timestamp::date) as errtotal
-#left join (select time::timestamp::date, count(*) as requests from log group by time::timestamp::date) as total on errtotal.time::timestamp::date = total.time::timestamp::date
-#where (cast(errtotal.error as float) / cast(total.requests as float) * 100) > 1;
+        print("{} - {:.2f}% errors".format(x[0].strftime("%B %d, %Y"), x[1]))
