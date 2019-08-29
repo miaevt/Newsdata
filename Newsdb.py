1b02359139932096140eb49d32a92a656c0c9e0b
Mia Moore <miaevt@gmail.com>
2019-08-28 21:00:06 -0500

    Change query lines on path equivalence and improve output format

--- Newsdb.py
+++ Newsdb.py
@@ -5,10 +5,10 @@ import psycopg2
 if __name__ == '__main__':
     DBNAME = "news"
     q1 = """select articles.title, logpath.views from logpath
-    join articles on logpath.path like concat('%', articles.slug, '%')
+    join articles on logpath.path = '/article/' || articles.slug
     order by logpath.views desc limit 3;"""
     q2 = """select authors.name, sum(logpath.views) as views from logpath
-    join articles on logpath.path like concat('%', articles.slug, '%')
+    join articles on logpath.path = '/article/' || articles.slug
     join authors on articles.author = authors.id
     group by authors.name order by views desc;"""
     q3 = """select error.time::date,
@@ -33,10 +33,10 @@ if __name__ == '__main__':
     for x in articles:
         print("{} - {} views".format(x[0], x[1]))
 
-    print("\nMost popular article authors of all time:\n")
+    print("\n\nMost popular article authors of all time:\n")
     for x in authors:
         print("{} - {} views".format(x[0], x[1]))
 
-    print("\nDays with more than 1% of error requests:\n")
+    print("\n\nDays with more than 1% of error requests:\n")
     for x in errors:
-        print("{} - {:.2f}% errors".format(x[0].strftime("%B %d, %Y"), x[1]))
+        print("{:%B %d, %Y} - {:.2f}% errors".format(x[0], x[1]))
